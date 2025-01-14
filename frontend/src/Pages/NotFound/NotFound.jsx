e6c007b8fd535e692d8272b4612296e00a12195e
TournierSebastian <110041924+TournierSebastian@users.noreply.github.com>
2025-01-14 14:19:46 -0300

    Page Not Found

--- frontend/src/Pages/NotFound/NotFound.jsx
+++ frontend/src/Pages/NotFound/NotFound.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
-
+import Logo from '../../Assets/icons/Logo.png'
+import Notfoundicon from '../../Assets/imagenes/404.png'
+import { Link } from 'react-router-dom'
 const NotFound = () => {
   return (
-    <div>Pagina no encontrada</div>
+    <div className='d-flex flex-column justify-content-center align-items-center'  style={{ height: '100vh' }}>
+        <Link to= '/' className='text-decoration-none d-flex  justify-content-center'>
+        <img src={Logo} className='w-50'></img>
+        </Link>
+        <img src={Notfoundicon} className='w-25 p-0 m-2'></img>
+
+        <div className='d-flex flex-column justify-content-center align-items-center fs-5'>
+        <p>Pagina no encontrada</p>
+
+        <button className='bg-Primary px-5 py-1 border-0 rounded-3'>
+        <Link to='/' className='SecondaryText text-decoration-none' >Inicio</Link>
+        </button>
+        </div>
+    </div>
   )
 }
 
