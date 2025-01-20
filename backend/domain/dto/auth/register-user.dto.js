a53e6f94c90232b936f0263f6b004468966ecb42
EduardoSalasG <eduardo.salasgo@correoaiep.cl>
2025-01-20 13:38:04 -0300

    feat(authService): refactor loginUser applying SOLID principles
    Refactored loginUser method in authService to improve code maintainability and readability by applying SOLID principles.
    
    Also added template.env files for backend and frontend to standardize environment variable configuration.
    
    Changes:
    - Extracted getUserByEmail, generateToken & comparePassword  method from loginUser to separate responsibilities.
    - Added template.env for both backend and frontend to improve development setup.

--- backend/domain/dto/auth/register-user.dto.js
+++ backend/domain/dto/auth/register-user.dto.js
@@ -1,27 +1,30 @@
 const { regularExps } = require('../../../config/regular-exp');
 
 
-class LoginUserDTO {
+class RegisterUserDTO {
 
     constructor(
+        name,
         email,
         password,
     ) {
+        this.name = name;
         this.email = email;
         this.password = password;
     }
 
     static create(object) {
 
-        const { email, password } = object;
+        const { name, email, password } = object;
 
+        if (!name) return ['Missing name'];
         if (!email) return ['Missing email'];
         if (!regularExps.email.test(email)) return ['Email is not valid email'];
         if (!password) return ['Missing password'];
         if (password.length < 6) return ['Password too short'];
 
-        return [undefined, new LoginUserDTO(email, password)]
+        return [undefined, new RegisterUserDTO(name, email, password)]
     }
 }
 
-module.exports = LoginUserDTO;
+module.exports = RegisterUserDTO;
--- backend/services/authService.js
+++ backend/services/authService.js
@@ -10,42 +10,43 @@ class AuthService {
 
     async getUserByEmail(email) {
 
-        try {
-            const user = await User.findOne({ where: { email } });
-            return console.log(user)
-            if (user) {
-                res.status(200).json(user);
-            } else {
-                res.status(404).json({ message: 'User not found' });
-            }
-        } catch (error) {
-            res.status(500).json({ message: error.message });
-        }
+        const user = await User.findOne({ where: { email } });
+        if (!user) throw CustomError.badRequest('Email does not exist');
+        return user
     };
 
-    async loginUser(loginUserDto) {
-        console.log('authservice, loginuser')
-        //find.one para verificar si existe
-        const user = await User.findOne({ where: { email: loginUserDto.email } });
-        console.log(user)
-        if (!user) throw CustomError.badRequest('Email does not exist');
+    comparePassword(encryptedPassword, plainPassword) {
 
-
-        //isMatch... bcrypt compare(123456, hash)
-        const passwordMatch = bcryptAdapter.compare(loginUserDto.password, user.password)
+        const passwordMatch = bcryptAdapter.compare(encryptedPassword, plainPassword)
         if (!passwordMatch) throw CustomError.badRequest('Password does not match');
+
+    }
+
+    async generateToken(user) {
+
+        const token = await JwtAdapter.generateToken({ id: user.ID_user, name: user.name });
+        if (!token) throw CustomError.internalServer('Error while creating JWT');
+        return token;
+
+    }
+
+    async loginUser(loginUserDto) {
+
+        const user = await this.getUserByEmail(loginUserDto.email);
+
+        this.comparePassword(loginUserDto.password, user.password)
 
         const { password, ...userEntity } = UserEntity.fromObject(user)
 
-        const token = await JwtAdapter.generateToken({ id: user.id });
-        if (!token) throw CustomError.internalServer('Error while creating JWT');
-
+        const token = await this.generateToken(userEntity);
 
         return {
             user: userEntity,
             token
         }
     }
+
+
 
 
 
