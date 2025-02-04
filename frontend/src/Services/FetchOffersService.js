93b1539dc84766749cc7f13f1d590298eb5d4524
EstebanRataLiendo <esteban.cba92@gmail.com>
2025-02-04 19:21:54 -0300

    MISOfertas Actualizadas

--- frontend/src/Services/FetchOffersService.js
+++ frontend/src/Services/FetchOffersService.js
@@ -1,15 +1,25 @@
-import { baseUrl, endpointsUrls } from "../constants.js"
 
-export const FetchOffersService = async() => {
-    const apiUrl = `${baseUrl}${endpointsUrls.RALL_JOB_OFFERS}/user/:userId`;
+import { baseUrl, endpointsUrls } from "../constants.js";
+
+export const FetchOffersService = async (userId) => {
+
+    const token = localStorage.getItem("Token"); 
+
+    if (!token) {
+        console.error("No hay token de autenticación");
+        return [];
+    }
+
+    const apiUrl = `${baseUrl}${endpointsUrls.RALL_JOB_OFFERS}/user/${userId}`;
+    
     try {
         const response = await fetch(apiUrl);
         if (!response.ok) {
             throw new Error("Error al obtener las ofertas de empleo");
         }
         const data = await response.json();
-        const filteredOffers = data.filter(offer => offer.User?.name === "ESTEBAN RATA LIENDO");
-        return filteredOffers;
+
+        return data;
     } catch (error) {
         console.error(error);
         return [];
