dabac4ae3877ce35aae75ff827c258008c3bf1e5
Maurolezcano81 <maurolezcano111@gmail.com>
2025-02-05 05:51:53 -0300

    Add dotenv urls on file constants

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -8,4 +8,8 @@ export const endpointsUrls = {
     RALL_CITIES: "/cities",
     RALL_JOB_CATEGORY: "/jobCategories",
     RALL_JOB_APLICATIONS_BY_OFFER: '/jobApplications/offer',
+    C_USER: "/auth/register",
+    R_ONE_USER_LOGIN: "/auth/login",
+    RALL_ROLES: '/roles',
+    RALL_COUNTRIES: '/countries'
 }
